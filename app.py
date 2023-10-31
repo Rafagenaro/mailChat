81ce69d9280389aef75f53ce94e09e0b7c8000fe
Guilherme <guilhermeosilva12@gmail.com>
2023-10-31 09:31:20 -0300

    [WIP] Work in progress

--- app.py
+++ app.py
@@ -1,23 +1,49 @@
-from flask import Flask, render_template, request, redirect, url_for
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_bcrypt import Bcrypt
 from models import User, db
 
 app = Flask(__name__, template_folder='templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:mvrg123@localhost/mailChat'
+app.config['SECRET_KEY'] = 'sua_chave_secreta_aqui'
 
 db.init_app(app)
 bcrypt = Bcrypt(app)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form['usuario']
+    senha = request.form['senha']
+    
+    user = User.query.filter_by(email=email).first()
+    
+    if user and bcrypt.check_password_hash(user.hash_senha, senha):
+        # O usuário está logado com sucesso
+        session['user_id'] = user.id
+        return redirect(url_for('index'))
+    else:
+        # Falha no login
+        return "Falha no login", 401
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('index'))
+
 @app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro():
     if request.method == "POST":
         hashed_senha = bcrypt.generate_password_hash(request.form['senha']).decode('utf-8')
+        data_nascimento = datetime.strptime(request.form['data_nascimento'], '%Y-%m-%d')
         user = User(nome=request.form['nome'], sobrenome=request.form['sobrenome'], email=request.form['email'], 
-                    hash_senha=hashed_senha, data_nascimento=request.form['data_nascimento'], 
+                    hash_senha=hashed_senha, data_nascimento=data_nascimento, 
                     sexo=request.form['sexo'])
         db.session.add(user)
         db.session.commit()
@@ -35,4 +61,3 @@ def create_db():
 
 if __name__ == '__main__':
     app.run()
-
--- models.py
+++ models.py
@@ -3,10 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(100), nullable=False)
     sobrenome = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     hash_senha = db.Column(db.String(60), nullable=False)
     data_nascimento = db.Column(db.Date, nullable=False)
     sexo = db.Column(db.String(50), nullable=False)
+
+    def __init__(self, nome, sobrenome, email, hash_senha, data_nascimento, sexo,  id=None):
+        self.id = id
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.email = email
+        self.hash_senha = hash_senha
+        self.data_nascimento = data_nascimento
+        self.sexo = sexo
