1e4871cbe11d0b13c95fe818e472fa8094061bb4
Guilherme <guilhermeosilva12@gmail.com>
2023-11-21 08:33:32 -0300

    Finalizado

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_bcrypt import Bcrypt
 from models import User, db
+from flask_mail import Mail, Message
+import smtplib
+import os
 
 app = Flask(__name__, template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:mvrg123@localhost/mailChat'
@@ -19,18 +23,49 @@ def index():
 
 @app.route('/login', methods=['POST'])
 def login():
-    email = request.form['usuario']
-    senha = request.form['senha']
+    error = None
+    if request.method == 'POST':
+        email = request.form['usuario']
+        senha = request.form['senha']
+        
+        user = User.query.filter_by(email=email).first()
+        
+        if user and bcrypt.check_password_hash(user.hash_senha, senha):
+            session['user_id'] = user.id
+            return redirect(url_for('logado'))
+        else:
+            error = "E-mail ou senha incorretos."
     
-    user = User.query.filter_by(email=email).first()
+    return render_template('index.html', error=error)
     
-    if user and bcrypt.check_password_hash(user.hash_senha, senha):
-        # O usuário está logado com sucesso
-        session['user_id'] = user.id
-        return redirect(url_for('index'))
+@app.route('/logado')
+def logado():
+    user_id = session.get('user_id')  # Obtenha o ID do usuário da sessão
+    if user_id is None:
+        # Se não há usuário logado, redireciona para a página de login
+        return redirect(url_for('login'))
+
+    logged_in_user = User.query.get(user_id)  # Obtenha o usuário logado
+        
+    
+    
+    if logged_in_user:
+        sobrenome_logado = logged_in_user.sobrenome
+        nome_logado = logged_in_user.nome  # Aqui está o nome do usuário logado
+        other_users = User.query.filter(User.id != user_id).all()
+
+        
+
+        return render_template('logado.html', sobrenome_logado=sobrenome_logado, nome_logado=nome_logado, other_users=other_users, logged_in_user=logged_in_user)
     else:
-        # Falha no login
-        return "Falha no login", 401
+        # Se o usuário não for encontrado, pode ser necessário lidar com isso (ex: redirecionar para login)
+        return redirect(url_for('login'))
+    
+
+
+
+
+
 
 @app.route('/logout')
 def logout():
